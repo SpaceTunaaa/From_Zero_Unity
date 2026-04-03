805617e447b0d279232b804a4363c7f95ac265b1
SpaceTunaaa <spacetuna40@gmail.com>
2026-04-03 08:35:16 -0600

    added simple level, adjusted movement, added rigid body

--- Assets/Controls/Movement_Input_Handler.cs
+++ Assets/Controls/Movement_Input_Handler.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class Movement_Input_Handler : MonoBehaviour
 {
     public Player_Movement input; // reference the script we generated
+    public Rigidbody rb;
+    public float movementSpeed = 0.01f;
 
     private void OnEnable()
    {
@@ -23,8 +25,14 @@ public class Movement_Input_Handler : MonoBehaviour
        Vector2 move = input.Player.Movement.ReadValue<Vector2>();
        if (move != Vector2.zero)
        {
-           transform.position += new Vector3(move.x, move.y, 0) * 0.01f;
+        //    transform.position += new Vector3(move.x, move.y, 0) * movementSpeed;
+           rb.linearVelocity = new Vector3(move.x * movementSpeed, move.y*movementSpeed, 0);
+
        }
+        else
+        {
+            rb.linearVelocity= Vector3.zero;
+        }
    }
 
 }
